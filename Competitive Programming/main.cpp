ff32bcccce8effd7388368a9eb688262573c1243
yueliang <yueliang21027@gmail.com>
2023-01-29 01:13:16 -0800

    Initial Commit

---
+++ Competitive Programming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Competitive Programming
+//
+//  Created by Brian Da Cruz on 1/29/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
