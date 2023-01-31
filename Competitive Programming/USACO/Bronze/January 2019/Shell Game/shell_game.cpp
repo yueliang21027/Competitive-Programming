563b2d639b095d610849d56ea6d140cf7fa654dd
yueliang <yueliang21027@gmail.com>
2023-01-30 23:42:46 -0800

    shell game setup

---
+++ Competitive Programming/USACO/Bronze/January 2019/Shell Game/shell_game.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Shell Game
+//
+//  Created by Brian Da Cruz on 1/30/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
